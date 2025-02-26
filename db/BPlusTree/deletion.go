90e6bd8bb25e90ec324064927bdee97cc2a2d918
Felix Hasel <felixhasel3@gmx.de>
2025-02-26 02:32:10 +0100

    added the functions needed for deletion (leafDelete, nodeMerge and nodeReplace2Links)

--- db/BPlusTree/deletion.go
+++ db/BPlusTree/deletion.go
@@ -1,18 +1,32 @@
 package BPlusTree
 
-// remove a key from a leaf node
-func leafDelete(new Node, old Node, idx uint16) {
+import "errors"
 
+// remove a key from a leaf node by skipping over it (this is possible due to copy-on-write)
+func leafDelete(new Node, old Node, idx uint16) {
+	new.setHeader(BTreeLeaf, old.numKeys()-1)
+	nodeCopyRange(new, old, 0, 0, idx)
+	nodeCopyRange(new, old, idx, idx+1, old.numKeys()-idx)
 }
 
 // merge 2 nodes into 1
-func nodeMerge(new Node, left Node, right Node) {
+func nodeMerge(new Node, left Node, right Node) error {
+	if left.getNodeType() != right.getNodeType() {
+		return errors.New("node types are not equal (nodeMerge)")
+	}
 
+	new.setHeader(left.getNodeType(), right.numKeys()+left.numKeys())
+	nodeCopyRange(new, left, 0, 0, left.numKeys())
+	nodeCopyRange(new, right, left.numKeys()+1, 0, right.numKeys())
+	return nil
 }
 
 // replace 2 adjacent links with 1
 func nodeReplace2Links(new Node, old Node, idx uint16, ptr uint64, key []byte) {
-
+	new.setHeader(BTreeInternal, old.numKeys()-1)
+	nodeCopyRange(new, old, 0, 0, idx-1)
+	nodeInsertKV(new, idx, ptr, key, nil)
+	nodeCopyRange(new, old, idx+1, idx+2, old.numKeys()-idx-2)
 }
 
 /*
@@ -25,7 +39,7 @@ func shouldMerge(tree *BTree, parent Node, idx uint16, updated Node) (int, Node)
 	if updated.nbytes() > MaxPageSize/4 {
 		return 0, Node{}
 	}
-	// checks if the node is not the left most node. If it isnt then it is merged with the left sibling
+	// checks if the node is not the left most node. If it isn't then it is merged with the left sibling
 	if idx > 0 {
 		sibling := Node(tree.get(parent.getPointer(idx - 1)))
 		mergedSize := sibling.nbytes() + updated.nbytes() - Header
@@ -33,14 +47,14 @@ func shouldMerge(tree *BTree, parent Node, idx uint16, updated Node) (int, Node)
 			return -1, sibling
 		}
 	}
-	// checks if the node is not the right most node. If it isnt then it is merged with the right sibling
+	// checks if the node is not the right most node. If it isn't then it is merged with the right sibling
 	if idx+1 < parent.numKeys() {
 		sibling := Node(tree.get(parent.getPointer(idx + 1)))
 		merged := sibling.nbytes() + updated.nbytes() - Header
 		if merged <= MaxPageSize {
-			return +1, sibling // right
+			return 1, sibling // right
 		}
 	}
-	//if no merge can happen because the other nodes are too big or it has no siblings, return 0
+	//if no merge can happen because the other nodes are too big, or it has no siblings, return 0
 	return 0, Node{}
 }
